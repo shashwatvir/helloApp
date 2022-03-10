17e287d3f27b9ca1ef8f3b005f58612c35fce6e2
shashwatvir <ind.shashwat@gmail.com>
2022-03-10 18:43:28 +0530

    Create source.java

---
+++ source.java
@@ -0,0 +1,6 @@
+public class source {
+    public static void main(String[] args) {
+		softwareDeveloper softwareDeveloper = new softwareDeveloper(1001, "Ishwar", "Java");
+		System.out.println(softwareDeveloper.getDetails());
+	}
+}
