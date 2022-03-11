3cbca3ac6bfeb863df4a80b9694d24f154d80ac9
shashwatvir <ind.shashwat@gmail.com>
2022-03-11 16:05:20 +0530

    Create oddnumber.java

---
+++ oddnumber.java
@@ -0,0 +1,9 @@
+public class oddnumber {
+	public static void main(String[] args) {
+        int num = 25; 
+		while( num >= 1) {
+          System.out.print (num + " ");
+          num = num - 2;
+        } 
+    }
+}
