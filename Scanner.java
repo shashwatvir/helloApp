ff4086a599a0b572eded047bd13aca688f10e2ee
shashwatvir <ind.shashwat@gmail.com>
2022-03-11 12:23:44 +0530

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,20 @@
+import java.io.InputStream;
+
+public class Scanner {
+
+    public Scanner(InputStream in) {
+    }
+
+    public int nextInt() {
+        return 0;
+    }
+
+    public double nextDouble() {
+        return 0;
+    }
+
+    public float nextFloat() {
+        return 0;
+    }
+
+}
