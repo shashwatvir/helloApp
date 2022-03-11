511a7323098d43231809860cf933586f03bf81c9
shashwatvir <ind.shashwat@gmail.com>
2022-03-11 12:23:40 +0530

    Create dataTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,12 @@
+public class dataTypes {
+
+   public static void main(String[] args) {
+        //don't change anything before this line.
+        double distance = 2.367;
+        long distanceInMetres = 2367;
+        
+        //don't change anything after this line.
+        System.out.println("Distance in km="+distance);
+        System.out.println("Distance in metres="+distanceInMetres);
+   }
+}
