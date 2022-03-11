d0b07674b0745f3059521c3920946d3b99024802
shashwatvir <ind.shashwat@gmail.com>
2022-03-11 12:23:59 +0530

    Create userinput.java

---
+++ userinput.java
@@ -0,0 +1,24 @@
+public class userinput {
+    public static void main(String[] args) {
+      int a;
+      int b;
+      int c;
+      Scanner input = new Scanner(null);
+  
+          System.out.println("Enter the value for a");
+  
+          a = input.nextInt();
+  
+          System.out.println("Enter the value for b");
+  
+          b = input.nextInt();
+          
+          System.out.println("Enter the value for c");
+          
+          c = input.nextInt();
+          
+          System.out.println(a*b+c);
+      }
+    }
+  
+  
