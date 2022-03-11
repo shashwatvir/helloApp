82c5b3d3a8a01ccc42a8755407df626bf799bd13
shashwatvir <ind.shashwat@gmail.com>
2022-03-11 12:23:31 +0530

    Create celsiustof.java

---
+++ celsiustof.java
@@ -0,0 +1,12 @@
+public class celsiustof {
+
+   public static void main(String[] args) {
+
+      int C;
+      float F;
+      Scanner input= new Scanner(System.in);
+      C=input.nextInt();
+      F=9*(float)C/5+32;
+      System.out.println(F);
+   }
+}
