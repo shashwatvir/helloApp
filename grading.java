4a81cf2047ef8aa0194e0b3b0ddb4347530f8b18
shashwatvir <ind.shashwat@gmail.com>
2022-03-11 16:05:47 +0530

    Create grading.java

---
+++ grading.java
@@ -0,0 +1,27 @@
+public class grading {
+
+   public static void main(String[] args) {
+
+       String name;
+       int marks;
+       Scanner input = new Scanner(System.in);
+       name = input.nextLine();
+       marks = input.nextInt();
+       if (marks > 100) {
+           System.out.println("The grade scored by " + name+" is A");
+       }
+       else if (marks<=100 && marks>80){
+           System.out.println("The grade scored by " + name+" is B");
+
+       }
+       else if (marks<=80 && marks>60){
+           System.out.println("The grade scored by " + name+" is C");
+       }
+       else if (marks<=60 && marks>40){
+           System.out.println("The grade scored by " + name+" is D");
+       }
+       else if (marks<=40){
+           System.out.println("The grade scored by " + name+" is E");
+       }
+   }
+}
