0abbc48e8e466e1586e8635170d33cec06b550f0
shashwatvir <ind.shashwat@gmail.com>
2022-03-11 12:23:20 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,14 @@
+public class operator {
+
+   public static void main(String[] args) {
+
+      int X,Y;
+      Scanner input= new Scanner(System.in);
+      X=input.nextInt();
+      Y=input.nextInt();
+      boolean test;
+      test=(X+Y)>30;
+      System.out.println("The statement said by the person is "+test);
+   }
+}
+
