5235769673fc96f062454db7dbfdc39b2b7ef2d6
shashwatvir <ind.shashwat@gmail.com>
2022-03-11 12:23:03 +0530

    Create arrays2.java

---
+++ arrays2.java
@@ -0,0 +1,9 @@
+class arrays2 {
+    public static void main(String[] args) {
+      String employeeNames[] = {"Henry", "Shane", "Sharma", "Watson"};
+  
+      System.out.println(employeeNames[0] + " " + employeeNames[2]);
+      System.out.println(employeeNames[1] + " " + employeeNames[3]);
+    }
+  }
+  
